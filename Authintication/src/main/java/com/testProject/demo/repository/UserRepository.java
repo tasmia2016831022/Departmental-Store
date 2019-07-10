740c56ca23f3b6cf31b43088bb40562e5dc2a226
Tasmia Alamgir <tasmia22@student.sust.edu>
2019-07-10 14:00:50 +0600

    Initial Configure

--- Authintication/src/main/java/com/testProject/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.testProject.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.testProject.demo.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
